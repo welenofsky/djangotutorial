4ef7a8ab9aaa3f60f9fe46064aaa8e7041ee1410
Justin Welenofsky <justin@welenofsky.com>
2014-09-26 11:23:27 -0700

    P4: Adding some views

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^polls/', include('polls.urls')),
+
+    # Namespace is to allow dynamic linking to polls.urls (ex: templates/polls/index.html)
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
--- polls/views.py
+++ polls/views.py
@@ -6,10 +6,8 @@ from polls.models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context =  {'latest_question_list': latest_question_list}
+    context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
-
-
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
