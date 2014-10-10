cae7b030d152069b1d77db5e4a0781b85965920f
Justin Welenofsky <justin@welenofsky.com>
2014-10-10 12:59:29 -0700

    Removed functional_tests, added unit tests for was_published_recently()

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+class QuestionMethodTests(TestCase):
+
+	def test_was_published_recently_with_future_question(self):
+		"""
+		was_published_recently() should return False for dates in the future
+		"""
+		time = timezone.now() + datetime.timedelta(days=30)
+		future_question = Question(pub_date=time)
+		self.assertEqual(future_question.was_published_recently(), False)
+
+	def test_was_published_recently_with_old_question(self):
+		"""
+		was_published_recently() should return False for dates in the past  
+		older than one day
+		"""
+		time = timezone.now() - datetime.timedelta(days=1)
+		past_question = Question(pub_date=time)
+		self.assertEqual(past_question.was_published_recently(), False)
+
+	def test_was_published_recently_with_recent_question(self):
+		"""
+		was_published_recently() should return True for questions that 
+		are not only and hour old
+		"""
+		time = timezone.now() - datetime.timedelta(hours=1)
+		recent_question = Question(pub_date=time)
+		self.assertEqual(recent_question.was_published_recently(), True)
